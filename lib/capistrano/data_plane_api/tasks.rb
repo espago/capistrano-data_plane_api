176eb46c6612c78330010606e80e24e4b9dfc510
Mateusz Drewniak <m.drewniak@espago.com>
2022-12-29 02:49:22 +0100

    v0.1.1
    Fix Rake tasks

--- lib/capistrano/data_plane_api/tasks.rb
+++ lib/capistrano/data_plane_api/tasks.rb
@@ -6,14 +6,14 @@ namespace :data_plane_api do
   namespace :server do
     desc "Set the server's admin state to DRAIN through the HAProxy Data Plane API"
     task :set_drain do
-      next if no_haproxy?
+      next if ::Capistrano::DataPlaneApi.no_haproxy?
 
-      ::Capistrano::DataPlaneApi.server_set_drain fetch(:stage), force: force_haproxy?
+      ::Capistrano::DataPlaneApi.server_set_drain fetch(:stage), force: ::Capistrano::DataPlaneApi.force_haproxy?
     end
 
     desc "Set the server's admin state to READY through the HAProxy Data Plane API"
     task :set_ready do
-      next if no_haproxy?
+      next if ::Capistrano::DataPlaneApi.no_haproxy?
 
       sleep 3
       ::Capistrano::DataPlaneApi.server_set_ready fetch(:stage)
@@ -21,7 +21,7 @@ namespace :data_plane_api do
 
     desc "Set the server's admin state to MAINT through the HAProxy Data Plane API"
     task :set_maint do
-      next if no_haproxy?
+      next if ::Capistrano::DataPlaneApi.no_haproxy?
 
       sleep 3
       ::Capistrano::DataPlaneApi.server_set_maint fetch(:stage), force: true
